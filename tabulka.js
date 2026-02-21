b668f500fdd036aaa53184730190edd3a059052a
Voppik <vopaleckytomas@gmail.com>
2026-02-21 19:47:58 +0100

    Update tabulka.js
    added poznamka for searching

--- tabulka.js
+++ tabulka.js
@@ -38,7 +38,7 @@ const loadData = async (searchTerm = '') => {
     // Pokud uživatel zadal hledaný text, přidáme SQL filtr
     if (searchTerm.trim() !== '') {
         // Hledá searchTerm v pojmu NEBO v kategorii (ignoruje velikost písmen)
-        query = query.or(`pojem.ilike.%${searchTerm}%,kategorie.ilike.%${searchTerm}%,vysvetleni.ilike.%${searchTerm}%`);
+        query = query.or(`pojem.ilike.%${searchTerm}%,kategorie.ilike.%${searchTerm}%,vysvetleni.ilike.%${searchTerm}%,poznamka.ilike.%${searchTerm}%`);
     }
 
     const { data, error } = await query;
@@ -172,5 +172,6 @@ document.getElementById('btn-remove').onclick = async () => {
 // Načte data hned po spuštění stránky
 
 loadData();
+
 
 
